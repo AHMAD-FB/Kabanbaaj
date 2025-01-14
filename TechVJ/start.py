c0a66d96b36075a528cd2075593535a08e6a5583
Ahmad-noob <kurd87709@gmail.com>
2025-01-14 18:55:08 +0300

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -61,15 +61,15 @@ async def send_start(client: Client, message: Message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
     buttons = [[
-        InlineKeyboardButton("❣️ Developer", url = "https://t.me/ahmed_sndee")
+        InlineKeyboardButton("❣️ گەشەپێدەر", url = "https://t.me/ahmed_sndee")
     ],[
-        InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/paivak'),
-        InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/paivak')
+        InlineKeyboardButton('🔍 جوین گوروپ', url='https://t.me/paivak'),
+        InlineKeyboardButton('🖇️ جوین کەنال', url='https://t.me/paivak')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(
         chat_id=message.chat.id, 
-        text=f"<b>👋 Hi {message.from_user.mention}, I am Save Restricted Content Bot, I can send you restricted content by its post link.\n\nFor downloading restricted content /login first.\n\nKnow how to use bot by - /help</b>", 
+        text=f"<b>👋 سلاڤ {message.from_user.mention}, کارێ بوتی ئەوە بو داونلودکرنا ( وێنە و ڤیدیو و دەنگ و فایل و هتد..) دناڤ وان گوروپ و کەنالاندا یێن کو تشت تێدا نە هێنە سەیڤ کرن ئەز دێ بوتە داونلود کەم برێکا ناردنا لینکێ پوستی.\n\nل دەست پێکێ فەرمانا /login داگرە و ئەکاونتا خو داخل بکە   .\n\nچەوانیا بکارئینانا بوتی  - /help</b>", 
         reply_markup=reply_markup, 
         reply_to_message_id=message.id
     )
