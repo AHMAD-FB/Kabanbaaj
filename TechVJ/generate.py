c6930eb9eaf45bb719aa050202b467ecff64d825
Ahmad-noob <kurd87709@gmail.com>
2025-01-16 20:49:14 +0300

    Update generate.py

--- TechVJ/generate.py
+++ TechVJ/generate.py
@@ -37,42 +37,42 @@ async def main(bot: Client, message: Message):
     user_id = int(message.from_user.id)
     phone_number_msg = await bot.ask(chat_id=user_id, text="<b>بەرێز ژمارا خو فرێکە دگەل کودێ وەلاتی.</b>\n<b>ب ڤی شێوەی:</b> <code>+9647501234567, +964xxxxxxxxxx</code>")
     if phone_number_msg.text=='/cancel':
-        return await phone_number_msg.reply('<b>process cancelled !</b>')
+        return await phone_number_msg.reply('<b>پروسە هەلوەشا !</b>')
     phone_number = phone_number_msg.text
     client = Client(":memory:", API_ID, API_HASH)
     await client.connect()
-    await phone_number_msg.reply("Sending OTP...")
+    await phone_number_msg.reply("ناردنا کودی...")
     try:
         code = await client.send_code(phone_number)
-        phone_code_msg = await bot.ask(user_id, "Please check for an OTP in official telegram account. If you got it, send OTP here after reading the below format. \n\nIf OTP is `12345`, **please send it as** `1 2 3 4 5`.\n\n**Enter /cancel to cancel The Procces**", filters=filters.text, timeout=600)
+        phone_code_msg = await bot.ask(user_id, "**سەحکە ئەکاونتا خو یا تێلیگرامی کودەک یێ هاتی وی کودی فرێکە دەمێ کودی فرێدکەی بلا فەراغ دناڤبەرا ژماراندا بیت  ب ڤی شێوەی </b>\n<b> ب ڤی شێوەی : 1 2 3 4 5**", filters=filters.text, timeout=600)
     except PhoneNumberInvalid:
-        await phone_number_msg.reply('`PHONE_NUMBER` **is invalid.**')
+        await phone_number_msg.reply('`PHONE_NUMBER` **نە دروستە**')
         return
     if phone_code_msg.text=='/cancel':
-        return await phone_code_msg.reply('<b>process cancelled !</b>')
+        return await phone_code_msg.reply('<b>پروسە هەلوەشا !</b>')
     try:
         phone_code = phone_code_msg.text.replace(" ", "")
         await client.sign_in(phone_number, code.phone_code_hash, phone_code)
     except PhoneCodeInvalid:
-        await phone_code_msg.reply('**OTP is invalid.**')
+        await phone_code_msg.reply('**خەلەتە ئەڤ کودە**')
         return
     except PhoneCodeExpired:
-        await phone_code_msg.reply('**OTP is expired.**')
+        await phone_code_msg.reply('**ئەڤ کودە یێ ب سەرڤە چوی**')
         return
     except SessionPasswordNeeded:
-        two_step_msg = await bot.ask(user_id, '**Your account has enabled two-step verification. Please provide the password.\n\nEnter /cancel to cancel The Procces**', filters=filters.text, timeout=300)
+        two_step_msg = await bot.ask(user_id, '**ژمارا نهێنی یا ئەکاونتا خو فرێکە.**', filters=filters.text, timeout=300)
         if two_step_msg.text=='/cancel':
-            return await two_step_msg.reply('<b>process cancelled !</b>')
+            return await two_step_msg.reply('<b>پروسە هەلوەشا !</b>')
         try:
             password = two_step_msg.text
             await client.check_password(password=password)
         except PasswordHashInvalid:
-            await two_step_msg.reply('**Invalid Password Provided**')
+            await two_step_msg.reply('**ژمارا نهێنی خەلەتە.**')
             return
     string_session = await client.export_session_string()
     await client.disconnect()
     if len(string_session) < SESSION_STRING_SIZE:
-        return await message.reply('<b>invalid session sring</b>')
+        return await message.reply('<b>سیشن نە دروستە</b>')
     try:
         user_data = await db.get_session(message.from_user.id)
         if user_data is None:
@@ -80,8 +80,8 @@ async def main(bot: Client, message: Message):
             await uclient.connect()
             await db.set_session(message.from_user.id, session=string_session)
     except Exception as e:
-        return await message.reply_text(f"<b>ERROR IN LOGIN:</b> `{e}`")
-    await bot.send_message(message.from_user.id, "<b>Account Login Successfully.\n\nIf You Get Any Error Related To AUTH KEY Then /logout first and /login again</b>")
+        return await message.reply_text(f"<b>خەلەتی ل چونەژورێدا:</b> `{e}`")
+    await bot.send_message(message.from_user.id, "<b>چونە ژەرا ئەکاونتی ب سەرکەفتیانە دەرباز بو نوکە فەرمانا /start کلیک بکە.</b>")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
