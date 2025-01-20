931fe954e6b087cf869f735d271b7fa54047d501
AHMAD-FB <124088270+AHMAD-FB@users.noreply.github.com>
2025-01-20 15:07:03 +0300

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -61,7 +61,7 @@ async def send_start(client: Client, message: Message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
     buttons = [[
-        InlineKeyboardButton("❣️ گەشەپێدەر", url = "https://t.me/ahmed_sndee")
+        InlineKeyboardButton("❣️ گەشەپێدەر", url = "https://t.me/ahmed_snde")
     ],[
         InlineKeyboardButton('🔍 جوین گوروپ', url='https://t.me/paivak'),
         InlineKeyboardButton('🖇️ جوین کەنال', url='https://t.me/paivak')
